5ce98cfab4cfad774b13ce4183f64c798da38ab1
256995 <256995@vutbr.cz>
2024-04-10 10:09:37 +0200

    add searching pattern

--- searching.py
+++ searching.py
@@ -24,10 +24,10 @@ def read_data(file_name, field):
 
 def linear_search(sequence, my_number):
     """
-    search number in sequence
+    search my_number in sequence
     :param sequence: list
     :param my_number: float
-    :return:
+    :return: results (dict), positions of my_number in sequence and total count
     """
     results = {"positions": [], "count": 0}
     for idx in range(len(sequence)):
@@ -37,11 +37,35 @@ def linear_search(sequence, my_number):
     return results
 
 
+def pattern_search(sequence, my_pattern):
+    """
+    search my_pattern in sequence
+    :param sequence: str
+    :param my_pattern: str
+    :return: result (set), (position of my_pattern in sequence)
+    """
+    idx = 0
+    result = set()
+    middle_idx = int(len(my_pattern) / 2) + 1   # prostřední index z my_pattern zaokrouhleno nahoru (pro sudé délky)
+    while idx < (len(sequence) - len(my_pattern) + 1):
+        index = 0
+        for char in my_pattern:
+            if char != sequence[idx + index]:
+                break
+            index += 1
+        if index == len(my_pattern):
+            result.add((idx + middle_idx))
+        idx += 1
+    return result
+
+
 def main():
     unordered_numbers = read_data("sequential.json", "unordered_numbers")
     print(unordered_numbers)
-    number_in_sequence = linear_search(unordered_numbers, 0)
-    print(number_in_sequence)
+    my_number_in_sequence = linear_search(unordered_numbers, 0)
+    print(my_number_in_sequence)
+    my_pattern_in_sequence = pattern_search(read_data("sequential.json", "dna_sequence"), "AAG")
+    print(my_pattern_in_sequence)
 
 
 if __name__ == '__main__':
